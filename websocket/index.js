1a77507a86c203455da96122a7f32d3d45407355
Lucas Pereira <2231100@my.ipleiria.pt>
2025-11-12 09:33:48 +0000

    tentar corrigir o erro

--- websocket/index.js
+++
@@ -1,9 +0,0 @@
-import { Server } from "socket.io"; 
-
-const io = new Server(3000, { /* options */ }); 
-
-io.on("connection", (socket) => { 
-    socket.on("echo",(msg)=>{ 
-    socket.emit("echo",msg); 
-    }); 
-}); 
